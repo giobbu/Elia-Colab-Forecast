2b222535c1ee9256da352af0d30a22bea853f598
giovanni.buroni <giovanni.buroni@inesctec.pt>
2024-06-18 11:19:12 +0100

    add buyer_resource_name

--- source/ensemble/stack_generalization/data_preparation/data_train_test.py
+++ source/ensemble/stack_generalization/data_preparation/data_train_test.py
@@ -1,19 +1,20 @@
 import pandas as pd
 
-def prepare_train_test_data(df_ensemble, df_val, df_test, start_predictions, max_lag):
+def prepare_train_test_data(buyer_resource_name, df_ensemble, df_val, df_test, start_predictions, max_lag):
     "Prepare train and test data for ensemble model"
     assert isinstance(df_ensemble, pd.DataFrame), "df_ensemble should be a DataFrame"
     assert isinstance(df_val, pd.DataFrame), "df_val should be a DataFrame"
     assert isinstance(df_test, pd.DataFrame), "df_test should be a DataFrame"
-    assert isinstance(start_predictions, pd.Timestamp), "start_predictions should be a Timestamp"
-    assert 'diff_norm_measured' in df_val.columns, "diff_norm_measured should be in df_val columns"
-    assert 'diff_norm_measured' in df_test.columns, "diff_norm_measured should be in df_test columns"
+    #assert isinstance(start_predictions, pd.Timestamp), "start_predictions should be a Timestamp"
+    assert 'diff_norm_' + buyer_resource_name in df_val.columns, "diff_norm_measured should be in df_val columns"
+    assert 'diff_norm_' + buyer_resource_name in df_test.columns, "diff_norm_measured should be in df_test columns"
     assert isinstance(max_lag, int), "max_lag should be an integer"
     assert max_lag > 0, "max_lag should be greater than 0"
+    col_name_buyer = 'diff_norm_' + buyer_resource_name
     df_train_ensemble = df_ensemble[df_ensemble.index < start_predictions].copy()
-    df_train_ensemble.loc[:, 'diff_norm_targ'] = df_val['diff_norm_measured'].values[max_lag:]
+    df_train_ensemble.loc[:, 'diff_norm_targ'] = df_val[col_name_buyer].values[max_lag:]
     df_test_ensemble = df_ensemble[df_ensemble.index >= start_predictions].copy()
-    df_test_ensemble.loc[:, 'diff_norm_targ'] = df_test['diff_norm_measured'].values
+    df_test_ensemble.loc[:, 'diff_norm_targ'] = df_test[col_name_buyer].values
     return df_train_ensemble, df_test_ensemble
 
 def get_numpy_Xy_train_test(df_train_ensemble, df_test_ensemble):
--- source/ensemble/stack_generalization/second_stage/create_data_second_stage.py
+++ source/ensemble/stack_generalization/second_stage/create_data_second_stage.py
@@ -33,19 +33,18 @@ def create_2stage_dataframe(df_train_ensemble, df_test_ensemble, y_train, y_test
     df_2stage = pd.concat([df_insample, df_outsample], axis=0)
     return df_2stage
 
-def create_var_ensemble_dataframe(quantiles, quantile_predictions_dict, df_test):
+def create_var_ensemble_dataframe(buyer_resource_name, quantiles, quantile_predictions_dict, df_test):
     " Create ensemble dataframe from quantile predictions."
     assert len(quantiles) == len(quantile_predictions_dict), "Length mismatch between quantiles and quantile predictions"
     assert df_test.shape[0] == len(quantile_predictions_dict[quantiles[0]]), "Length mismatch between test data and predictions"
     for i, quantile in enumerate(quantiles):
         if i == 0:
             df_pred_ensemble = pd.DataFrame(quantile_predictions_dict[quantile])
-            df_pred_ensemble.columns = ['datetime', str(int(quantile*100))+'_var_predictions']
+            df_pred_ensemble.columns = ['datetime', 'q' + str(int(quantile*100)) + '_' + buyer_resource_name]
             df_pred_ensemble.set_index('datetime', inplace=True)
         else:
             df_pred_quantile = pd.DataFrame(quantile_predictions_dict[quantile])
-            df_pred_quantile.columns = ['datetime', str(int(quantile*100))+'_var_predictions']
+            df_pred_quantile.columns = ['datetime', 'q' + str(int(quantile*100)) + '_' + buyer_resource_name]
             df_pred_quantile.set_index('datetime', inplace=True)
             df_pred_ensemble = pd.concat([df_pred_ensemble, df_pred_quantile], axis=1)
-    df_pred_ensemble['targets'] = df_test['targets']
     return df_pred_ensemble
