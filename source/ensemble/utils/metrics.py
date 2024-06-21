95c2fb6301015aad559e0c478f6801bd81c6d224
giovanni.buroni <giovanni.buroni@inesctec.pt>
2024-06-21 16:06:54 +0100

    change column name in metrics functions

--- source/ensemble/utils/metrics.py
+++ source/ensemble/utils/metrics.py
@@ -7,7 +7,7 @@ def rmse(y_pred, y_targ):
     assert len(y_pred) == len(y_targ), "Length mismatch between predictions and targets"
     return np.sqrt(np.mean((y_pred - y_targ)**2))
 
-def calculate_rmse(df, pred_col, targ_col='diff_norm_measured'):
+def calculate_rmse(df, pred_col, targ_col='target'):
     " Calculate RMSE Loss."
     assert pred_col in df.columns, "prediction column is missing"
     assert targ_col in df.columns, "target column is missing"
@@ -15,9 +15,9 @@ def calculate_rmse(df, pred_col, targ_col='diff_norm_measured'):
     rmse_loss['rmse'] = np.array([rmse(df[pred_col], df[targ_col])])
     return rmse_loss
 
-def calculate_pinball_losses(df, confidence_10_col, confidence_90_col, targ_col='diff_norm_measured'):
+def calculate_pinball_losses(df, confidence_10_col, confidence_90_col, targ_col='target'):
     " Calculate Pinball Losses for 10% and 90% quantiles."
-    assert 'diff_norm_measured' in df.columns, "diff_norm_measured column is missing"
+    assert 'target' in df.columns, "target column is missing"
     pinball_losses = pd.DataFrame()
     score_10 = mean_pinball_loss( list(df[targ_col].values),  list(df[confidence_10_col].values), alpha=0.1)
     score_90 = mean_pinball_loss( list(df[targ_col].values),  list(df[confidence_90_col].values), alpha=0.9)
