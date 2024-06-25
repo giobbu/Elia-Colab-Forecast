fc9b6cf1b9e3c228102a5e6c1f719ce6d8d9ae92
giovanni.buroni <giovanni.buroni@inesctec.pt>
2024-06-25 19:44:52 +0100

    reorganize module

--- source/ensemble/stack_generalization/second_stage/create_data_second_stage.py
+++ source/ensemble/stack_generalization/second_stage/create_data_second_stage.py
@@ -1,22 +1,5 @@
 import pandas as pd
 
-def create_augmented_dataframe_2stage(df_2stage, order_diff, max_lags, augment=False):
-    " Process 2-stage ensemble dataframe with lags."
-    assert order_diff > 0, "Order of differentiation must be greater than 0"
-    assert max_lags > 0, "Maximum number of lags must be greater than 0"
-    # Differentiate the dataframe
-    df_2stage_diff = df_2stage.diff(order_diff)
-    # Create lagged features
-    for lag in range(1, max_lags + 1):
-        df_2stage_diff[f'predictions_t-{lag}'] = df_2stage_diff['predictions'].shift(lag)
-    if augment:
-        for col in df_2stage_diff.columns:
-            if 'targets' not in col:
-                df_2stage_diff[f'{col}_sqr'] = df_2stage_diff[col]**2
-    # Drop rows with NaNs resulting from the shift operation
-    df_2stage_process = df_2stage_diff.iloc[max_lags+1:, :]
-    return df_2stage_process
-
 def create_2stage_dataframe(df_train_ensemble, df_test_ensemble, y_train, y_test, predictions_insample, predictions_outsample):
     assert df_train_ensemble.shape[0] == len(predictions_insample), "Length mismatch between train data and in-sample predictions"
     assert df_test_ensemble.shape[0] == len(predictions_outsample), "Length mismatch between test data and out-sample predictions"
@@ -32,6 +15,23 @@ def create_2stage_dataframe(df_train_ensemble, df_test_ensemble, y_train, y_test
     # Concatenating in-sample and out-sample DataFrames
     df_2stage = pd.concat([df_insample, df_outsample], axis=0)
     return df_2stage
+
+def create_augmented_dataframe_2stage(df_2stage, order_diff, max_lags, augment=False):
+    " Process 2-stage ensemble dataframe with lags."
+    assert order_diff > 0, "Order of differentiation must be greater than 0"
+    assert max_lags > 0, "Maximum number of lags must be greater than 0"
+    # Differentiate the dataframe
+    df_2stage_diff = df_2stage.diff(order_diff)
+    # Create lagged features
+    for lag in range(1, max_lags + 1):
+        df_2stage_diff[f'predictions_t-{lag}'] = df_2stage_diff['predictions'].shift(lag)
+    if augment:
+        for col in df_2stage_diff.columns:
+            if 'targets' not in col:
+                df_2stage_diff[f'{col}_sqr'] = df_2stage_diff[col]**2
+    # Drop rows with NaNs resulting from the shift operation
+    df_2stage_process = df_2stage_diff.iloc[max_lags+order_diff:, :]
+    return df_2stage_process
 
 def create_var_ensemble_dataframe(buyer_resource_name, quantiles, quantile_predictions_dict, df_test):
     " Create ensemble dataframe from quantile predictions."
