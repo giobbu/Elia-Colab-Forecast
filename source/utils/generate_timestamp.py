b795394a4f745be239b75f3240ecbe628ac1aa63
giovanni.buroni <giovanni.buroni@inesctec.pt>
2024-06-28 11:36:02 +0100

    fix bug to generate forecasts day-ahead

--- source/utils/generate_timestamp.py
+++ source/utils/generate_timestamp.py
@@ -5,6 +5,6 @@ def generate_timestamps(start_training, i, window_size):
     assert window_size > 0, "Window size must be greater than 0"
     start_training_timestamp = pd.to_datetime(start_training, utc=True) + pd.Timedelta(days=i)
     end_training_timestamp = pd.to_datetime(start_training, utc=True) + pd.Timedelta(days=i + window_size)
-    start_prediction_timestamp = pd.to_datetime(start_training, utc=True) + pd.Timedelta(days=i + window_size)
-    end_prediction_timestamp = pd.to_datetime(start_training, utc=True) + pd.Timedelta(days=i + window_size + 1)
+    start_prediction_timestamp = pd.to_datetime(start_training, utc=True) + pd.Timedelta(days=1 + i + window_size)
+    end_prediction_timestamp = pd.to_datetime(start_training, utc=True) + pd.Timedelta(days=2 + i + window_size)
     return start_training_timestamp, end_training_timestamp, start_prediction_timestamp, end_prediction_timestamp
