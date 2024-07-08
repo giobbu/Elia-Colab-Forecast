3a757052b62b5c2adb5f813317bf78d410fe16f4
giovanni.buroni <giovanni.buroni@inesctec.pt>
2024-07-08 11:56:55 +0100

    add scenarios to simulation

--- config/simulation_setting.py
+++ config/simulation_setting.py
@@ -2,22 +2,30 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class Simulation:
-    testing_period = dict( 
+    testing_period = dict(
         file_0 = '/Users/gio/Desktop/elia_group/dataset_elia/2023/01.json',
         file_1 = '/Users/gio/Desktop/elia_group/dataset_elia/2023/02.json',
         file_2 = '/Users/gio/Desktop/elia_group/dataset_elia/2023/03.json',
         file_3 = '/Users/gio/Desktop/elia_group/dataset_elia/2023/04.json',
         file_4 = '/Users/gio/Desktop/elia_group/dataset_elia/2023/05.json',
+        random_seed = 42,
         window_size = 30,
-        start_training = '2023-02-25',
-        num_test_days = 80,
+        start_training = '2023-01-01',
+        num_test_days = 60,
         forecasts_col = ['forecast', 'confidence10', 'confidence90'],
         measured_col = 'measured',
+        most_recent = True,
+        malicious = False,
+        malicious_name= 'mostrecent',
+        noise_degree = 10,
+        noisy = False,
+        noisy_name = 'weekahead',
+        scenario = 'malicious',
         display_metrics=True)
     
 @dataclass(frozen=True)
 class WeightedAvg:
-    params = dict(window_size_valid = 5)
+    params = dict(window_size_valid = 3)
 
 @dataclass(frozen=True)
 class Stack:
@@ -28,23 +36,23 @@ class Stack:
         
         normalize = True,
         compute_abs_difference = False,
-        add_quantile_predictions = False,
-        augment_q50 = False,
+        add_quantile_predictions = True,
+        augment_q50 = True,
 
         # prediction pipeline
-        nr_cv_splits = 5,
+        nr_cv_splits = 10,
         quantiles = [0.1, 0.9, 0.5],
 
         # params for 1st stage
         max_lags = 3,
         forecasters_diversity = False,
         lagged = True,
-        augment = False,
+        augment = True,
         differenciate = False,
 
         # params for 2nd stage
         max_lags_var = 3,
-        augment_var=False,
+        augment_var=True,
 
         baseline_model = 'diff_norm_dayahead',
 
@@ -52,30 +60,31 @@ class Stack:
         model_type = 'LR',  # 'GBR' or 'LR'
         var_model_type = 'LR',  # 'GBR' or 'LR'
 
-        gbr_update_every_days = 30,
+        gbr_update_every_days = 15,
 
         # forecasts model parameters
-        gbr_config_params = {'learning_rate': [0.001, 0.005, 0.01],
-                                'max_features' : [.85, .95, 1.0],
-                                'max_depth': [3, 4],
-                                'max_iter': [250, 500]},
-        lr_config_params = {'alpha': [0.00001, 0.0001, 0.001, 0.005],
+        gbr_config_params = {'learning_rate': [0.0001, 0.001, 0.005, 0.01],
+                                'max_features' : [.98, 1.0],
+                                'max_depth': [3, 4, 5],
+                                'max_iter': [250, 500, 1000]},
+        lr_config_params = {'alpha': [0, 0.0000001, 0.000001, 0.00001, 0.0001, 0.001, 0.005],
                             'fit_intercept' : [True, False]},
 
         # variability forecasts model parameters
         order_diff = 1,
-        var_gbr_config_params = {'learning_rate': [0.001, 0.005, 0.01],
-                                    'max_features' : [.85, .95, 1.0],
-                                    'max_depth': [3, 4],
-                                    'max_iter': [150, 300]},
-        var_lr_config_params = {'alpha': [0.00001, 0.0001, 0.001, 0.005],
+        var_gbr_config_params = {'learning_rate': [0.0001, 0.001, 0.005, 0.01],
+                                    'max_features' : [.98, 1.0],
+                                    'max_depth': [3, 4, 5],
+                                    'max_iter': [150, 500]},
+        var_lr_config_params = {'alpha': [0, 0.0000001, 0.000001, 0.00001, 0.0001, 0.001, 0.005],
                                 'fit_intercept' : [True, False]},
 
-        nr_permutations=100,
+        nr_permutations=10,
         compute_second_stage = True,  # activate the second stage of the ensemble learning
 
         # plot settings
-
+        plt_wind_power_ensemble = True,  # plot the wind power ensemble forecasts
+        plt_wind_power_variability_ensemble = True,  # plot the wind power variability forecasts
         plot_baseline = False,  # plot the baseline model
         plot_weighted_avg = False,  # plot the weighted average predictions
         plot_importance_gbr = False,  # plot the feature importances for the GBR model
