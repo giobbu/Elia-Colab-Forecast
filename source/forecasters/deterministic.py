5667cfbe45c4f67027b878f44072f2e7322869e5
giovanni.buroni <giovanni.buroni@inesctec.pt>
2024-07-08 12:35:06 +0100

    add forecasters scenario cases

--- source/forecasters/deterministic.py
+++ source/forecasters/deterministic.py
@@ -1,4 +1,53 @@
 from source.utils.ensemble_predictions import dict2df_predictions
+import numpy as np
+from config.simulation_setting import Simulation
+sim_params = Simulation.testing_period
+
+
+# noisy
+def create_noisy_predictions(df, column):
+    " Create most recent predictions with cheat option "
+    assert df.index.name == 'datetime', "Index must be datetime"
+    # column string contained in column forecast
+    assert f'{column}forecast' in df.columns, f"{column}forecast column must be present"
+    noisy_prediction = []
+    for i in range(len(df)):
+        noisy_prediction.append({'datetime': df.index[i],
+                                    'predictions': df[f'{column}forecast'].sample(1).iloc[0]})
+    dfnoisy_pred = dict2df_predictions(noisy_prediction, 'noisy')
+    return dfnoisy_pred
+
+# malicious
+def create_malicious_predictions(df, column, cheat=False, df_train=None):
+    " Create most recent predictions with cheat option "
+    assert df.index.name == 'datetime', "Index must be datetime"
+    # column string contained in column forecast
+    assert f'{column}forecast' in df.columns, f"{column}forecast column must be present"
+    cheat_prediction = []
+    for i in range(len(df)):
+        if not cheat:
+            cheat_prediction.append({'datetime': df.index[i],
+                                        'predictions': df[f'{column}forecast'].iloc[i] + sim_params['noise_degree']*np.random.normal(0, 1) })
+        elif cheat:
+            assert df_train is not None, "df_train must be provided"
+            cheat_prediction.append({'datetime': df.index[i],
+                                        'predictions': df_train[f'{column}forecast'].sample(1).iloc[0] + sim_params['noise_degree']*np.random.normal(0, 1)})
+        else:
+            raise ValueError("cheat must be either True or False")
+    dfcheat_pred = dict2df_predictions(cheat_prediction, 'malicious')
+    return dfcheat_pred
+
+# most recent
+def create_most_recent_predictions(df_val):
+    " Create most recent predictions "
+    assert df_val.index.name == 'datetime', "Index must be datetime"
+    assert 'mostrecentforecast' in df_val.columns, "dayaheadforecast column must be present"
+    most_recent_elia_prediction = []
+    for i in range(len(df_val)):
+        most_recent_elia_prediction.append({'datetime': df_val.index[i],
+                                            'predictions': df_val.mostrecentforecast.iloc[i]})
+    df_most_recent_pred = dict2df_predictions(most_recent_elia_prediction, 'most_recent')
+    return df_most_recent_pred
 
 # day ahead
 def create_day_ahead_predictions(df_val):
--- source/forecasters/probabilistic.py
+++ source/forecasters/probabilistic.py
@@ -1,4 +1,50 @@
 from source.utils.ensemble_predictions import dict2df_quantiles10, dict2df_quantiles90
+import numpy as np
+from config.simulation_setting import Simulation
+sim_params = Simulation.testing_period
+
+# noisy
+def create_noisy_quantiles10(df, column):
+    " Create most recent quantiles 10 with cheat option "
+    assert df.index.name == 'datetime', "Index must be datetime"
+    assert f'{column}confidence10' in df.columns, f"{column}confidence10 column must be present"
+    noisy_prediction = []
+    for i in range(len(df)):
+        noisy_prediction.append({'datetime': df.index[i],
+                                    'quantiles10': df[f'{column}confidence10'].sample(1).iloc[0]})
+    dfnoisy_pred = dict2df_quantiles10(noisy_prediction, 'noisy')
+    return dfnoisy_pred
+
+# malicious
+def create_malicious_quantiles10(df, column, cheat=False, df_train=None):
+    " Create most recent quantiles 10 with cheat option "
+    assert df.index.name == 'datetime', "Index must be datetime"
+    assert f'{column}confidence10' in df.columns, f"{column}confidence10 column must be present"
+    cheat_prediction = []
+    for i in range(len(df)):
+        if not cheat:
+            cheat_prediction.append({'datetime': df.index[i],
+                                        'quantiles10': df[f'{column}confidence10'].iloc[i] + sim_params['noise_degree']*np.random.normal(0, 1)})
+        elif cheat:
+            assert df_train is not None, "df_train must be provided"
+            cheat_prediction.append({'datetime': df.index[i],
+                                        'quantiles10': df_train[f'{column}confidence10'].sample(1).iloc[0] + sim_params['noise_degree']*np.random.normal(0, 1)})
+        else:
+            raise ValueError("cheat must be either True or False")
+    dfcheat_pred = dict2df_quantiles10(cheat_prediction, 'malicious')
+    return dfcheat_pred
+
+# most recent
+def create_most_recent_quantiles10(df_val):
+    " Create most recent quantiles 10"
+    assert df_val.index.name == 'datetime', "Index must be datetime"
+    assert 'mostrecentconfidence10' in df_val.columns, "mostrecentconfidence10 column must be present"
+    most_recent_elia_quantile10 = []
+    for i in range(len(df_val)):
+        most_recent_elia_quantile10.append({'datetime': df_val.index[i],
+                                            'quantiles10': df_val.mostrecentconfidence10.iloc[i]})
+    df_most_recent_quantile10 = dict2df_quantiles10(most_recent_elia_quantile10, 'most_recent')
+    return df_most_recent_quantile10
 
 # day ahead
 def create_day_ahead_quantiles10(df_val):
@@ -35,6 +81,51 @@ def create_week_ahead_quantiles10(df_val):
                                             'quantiles10': df_val.weekaheadconfidence10.iloc[i]})
     df_week_ahead_quantile10 = dict2df_quantiles10(week_ahead_elia_quantile10, 'week_ahead')
     return df_week_ahead_quantile10
+
+
+# noisy
+def create_noisy_quantiles90(df, column):
+    " Create most recent quantiles 90 with cheat option "
+    assert df.index.name == 'datetime', "Index must be datetime"
+    assert f'{column}confidence90' in df.columns, f"{column}confidence90 column must be present"
+    noisy_prediction = []
+    for i in range(len(df)):
+        noisy_prediction.append({'datetime': df.index[i],
+                                    'quantiles90': df[f'{column}confidence90'].sample(1).iloc[0]})
+    dfnoisy_pred = dict2df_quantiles90(noisy_prediction, 'noisy')
+    return dfnoisy_pred
+
+# malicous
+def create_malicious_quantiles90(df, column, cheat=False, df_train=None):
+    " Create most recent quantiles 90 with cheat option "
+    assert df.index.name == 'datetime', "Index must be datetime"
+    assert f'{column}confidence90' in df.columns, f"{column}confidence90 column must be present"
+    cheat_prediction = []
+    for i in range(len(df)):
+        if not cheat:
+            cheat_prediction.append({'datetime': df.index[i],
+                                        'quantiles90': df[f'{column}confidence90'].iloc[i] + sim_params['noise_degree']*np.random.normal(0, 1)})
+        elif cheat:
+            assert df_train is not None, "df_train must be provided"
+            cheat_prediction.append({'datetime': df.index[i],
+                                        'quantiles90': df_train[f'{column}confidence90'].sample(1).iloc[0] + sim_params['noise_degree']*np.random.normal(0, 1)})
+        else:
+            raise ValueError("cheat must be either True or False")
+    dfcheat_pred = dict2df_quantiles90(cheat_prediction, 'malicious')
+    return dfcheat_pred
+
+
+# most recent
+def create_most_recent_quantiles90(df_val):
+    " Create most recent quantiles 90"
+    assert df_val.index.name == 'datetime', "Index must be datetime"
+    assert 'mostrecentconfidence90' in df_val.columns, "mostrecentconfidence90 column must be present"
+    most_recent_elia_quantile90 = []
+    for i in range(len(df_val)):
+        most_recent_elia_quantile90.append({'datetime': df_val.index[i],
+                                            'quantiles90': df_val.mostrecentconfidence90.iloc[i]})
+    df_most_recent_quantile90 = dict2df_quantiles90(most_recent_elia_quantile90, 'most_recent')
+    return df_most_recent_quantile90
 
 # day ahead
 def create_day_ahead_quantiles90(df_val):
