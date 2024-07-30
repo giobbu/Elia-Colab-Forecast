4a18f497ae32b0e023736bed8dccaf7544ac34ed
giovanni.buroni <giovanni.buroni@inesctec.pt>
2024-07-30 12:13:16 +0100

    refactor claculate_equal_weights

--- source/ensemble/combination_scheme/equal_weights.py
+++ source/ensemble/combination_scheme/equal_weights.py
@@ -4,15 +4,12 @@ def calculate_equal_weights(df_test_norm):
     " Calculate the mean prediction and quantiles using equal weights"
     assert 'norm_measured' in df_test_norm.columns, "norm_measured column is missing"
     assert len(df_test_norm) == 96, "Dataframe must have 96 rows"
-    Q10 = df_test_norm[['norm_weekaheadconfidence10', 
-                                'norm_dayaheadconfidence10', 
-                                'norm_dayahead11hconfidence10']].mean(axis=1)
-    MEAN = df_test_norm[['norm_weekaheadforecast', 
-                                'norm_dayaheadforecast', 
-                                'norm_dayahead11hforecast']].mean(axis=1)
-    Q90 = df_test_norm[['norm_weekaheadconfidence90', 
-                                'norm_dayaheadconfidence90', 
-                                'norm_dayahead11hconfidence90']].mean(axis=1)
+    list_q10 = [name for name in list(df_test_norm.columns) if 'confidence10' in name]
+    list_q50 = [name for name in list(df_test_norm.columns) if 'forecast' in name]
+    list_q90 = [name for name in list(df_test_norm.columns) if 'confidence90' in name]
+    Q10 = df_test_norm[list_q10].mean(axis=1)
+    MEAN = df_test_norm[list_q50].mean(axis=1)
+    Q90 = df_test_norm[list_q90].mean(axis=1)
     df_equal_weights = pd.DataFrame({
         'Q10': Q10,
         'mean_prediction': MEAN,
