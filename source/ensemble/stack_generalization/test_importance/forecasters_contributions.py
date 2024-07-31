446c4702c9aac71d5afc5f8416d066f598a7dffc
giovanni.buroni <giovanni.buroni@inesctec.pt>
2024-07-31 15:59:33 +0100

    add y_test 1stage/2stage

--- source/ensemble/stack_generalization/test_importance/forecasters_contributions.py
+++ source/ensemble/stack_generalization/test_importance/forecasters_contributions.py
@@ -20,12 +20,15 @@ def calculate_contributions(results_challenge_dict, ens_params, y_test, forecast
     assert isinstance(results_challenge_dict, dict), 'The results_challenge_dict must be a dictionary'
     assert isinstance(ens_params, dict), 'The ens_params must be a dictionary'
     assert isinstance(y_test, np.ndarray), 'The y_test must be a numpy array'
+    assert len(y_test) >= 192, 'The length of y_test must be at least 192'
     results_contributions = defaultdict(dict)
     try:
         # wind power importance
-        results_contributions = wind_power_importance(results_challenge_dict, ens_params, y_test, results_contributions)
+        y_test_1st_stage = y_test[-96:]
+        results_contributions = wind_power_importance(results_challenge_dict, ens_params, y_test_1st_stage, results_contributions)
         # wind power ramp importance
-        results_contributions = wind_power_ramp_importance(results_challenge_dict, ens_params, y_test, previous_day_forecast_range, results_contributions)
+        y_test_2nd_stage = y_test[-192:]
+        results_contributions = wind_power_ramp_importance(results_challenge_dict, ens_params, y_test_2nd_stage, forecast_range, results_contributions)
     except Exception as e:
         logger.error(f"Failed to calculate contributions: {e}")
     return results_contributions
