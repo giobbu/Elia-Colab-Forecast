f4a956c854a3afd180409d2cc7bec47b0f003f53
giovanni.buroni <giovanni.buroni@inesctec.pt>
2024-10-04 11:18:48 +0100

    update simulation setting

--- config/simulation_setting.py
+++ config/simulation_setting.py
@@ -6,26 +6,36 @@ solver = "highs" if sp_version >= parse_version("1.6.0") else "interior-point"
 @dataclass(frozen=True)
 class Simulation:
     testing_period = dict(
-        file_0 = '/Users/gio/Desktop/elia_group/dataset_elia/2023/01.json',
-        file_1 = '/Users/gio/Desktop/elia_group/dataset_elia/2023/02.json',
-        file_2 = '/Users/gio/Desktop/elia_group/dataset_elia/2023/03.json',
-        file_3 = '/Users/gio/Desktop/elia_group/dataset_elia/2023/04.json',
-        file_4 = '/Users/gio/Desktop/elia_group/dataset_elia/2023/05.json',
+        file_1 = '/Users/gio/Desktop/elia_group/dataset_elia/2023/01.json',
+        file_2 = '/Users/gio/Desktop/elia_group/dataset_elia/2023/02.json',
+        file_3 = '/Users/gio/Desktop/elia_group/dataset_elia/2023/03.json',
+        file_4 = '/Users/gio/Desktop/elia_group/dataset_elia/2023/04.json',
+        file_5 = '/Users/gio/Desktop/elia_group/dataset_elia/2023/05.json',
+        file_6 = '/Users/gio/Desktop/elia_group/dataset_elia/2023/06.json',
+        file_7 = '/Users/gio/Desktop/elia_group/dataset_elia/2023/07.json',
+        file_8 = '/Users/gio/Desktop/elia_group/dataset_elia/2023/08.json',
+        file_9 = '/Users/gio/Desktop/elia_group/dataset_elia/2023/09.json',
+        file_10 = '/Users/gio/Desktop/elia_group/dataset_elia/2023/10.json',
+        file_11 = '/Users/gio/Desktop/elia_group/dataset_elia/2023/11.json',
+        file_12 = '/Users/gio/Desktop/elia_group/dataset_elia/2023/12.json',
+        replace_nan = True,
         random_seed = 42,
         window_size = 30,
         start_training = '2023-01-01',
-        num_test_days = 14,
+        num_test_days = 240,
         forecasts_col = ['forecast', 'confidence10', 'confidence90'],
         measured_col = 'measured',
         most_recent = True,
         malicious = False,
         malicious_name= 'mostrecent',
         noise_degree = 10,
-        noisy = True,
+        noisy = False,
         noisy_name = 'weekahead',
         scenario = 'malicious',
         save_scenario_contributions = False,
-        display_metrics=True)
+        display_metrics=True,
+        baselines_comparison = True,
+        contribution_comparison = False)
     
 @dataclass(frozen=True)
 class WeightedAvg:
@@ -49,7 +59,7 @@ class Stack:
         augment_q50 = True,
 
         # prediction pipeline
-        nr_cv_splits = 10,
+        nr_cv_splits = 5,
         quantiles = [0.1, 0.9, 0.5],
 
         # params for 1st stage
@@ -66,17 +76,21 @@ class Stack:
         baseline_model = 'diff_norm_dayahead',
 
         # Ensemble Learning
-        model_type = 'LR',  # 'GBR' or 'LR'
+        model_type = 'LR', #'LR',  # 'GBR' or 'LR'
         var_model_type = 'LR',  # 'GBR' or 'LR'
         solver = solver,
 
         gbr_update_every_days = 15,
 
+        conformalized_qr = False, # conformalized quantile regression
+        day_calibration = 2,
+
         # forecasts model parameters
         gbr_config_params = {'learning_rate': [0.0001, 0.001, 0.005, 0.01],
                                 'max_features' : [.98, 1.0],
                                 'max_depth': [3, 4, 5],
-                                'max_iter': [25]},
+                                'max_iter': [200, 300]},
+
         lr_config_params = {'alpha': [0, 0.0000001, 0.000001, 0.00001, 0.0001, 0.001, 0.005, 0.0075, 0.01],
                             'fit_intercept' : [True, False]},
 
@@ -85,12 +99,18 @@ class Stack:
         var_gbr_config_params = {'learning_rate': [0.0001, 0.001, 0.005, 0.01],
                                     'max_features' : [.98, 1.0],
                                     'max_depth': [3, 4, 5],
-                                    'max_iter': [25]},
+                                    'max_iter': [25, 50]},
         var_lr_config_params = {'alpha': [0, 0.0000001, 0.000001, 0.00001, 0.0001, 0.001, 0.005, 0.0075, 0.01],
                                 'fit_intercept' : [True, False]},
 
+        contribution_method = 'shapley',  # 'shapley' or 'permutation'
+
+        # permutation importance
         nr_permutations=10,
-        compute_second_stage = True,  # activate the second stage of the ensemble learning
+
+        # shapeley importance
+        nr_row_permutations = 5,
+        nr_col_permutations = 5,
 
         # plot settings
         plt_wind_power_ensemble = True,  # plot the wind power ensemble forecasts
