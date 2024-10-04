9a5685f256ec538360efa9aedd6fe6b7bed9fb6e
giovanni.buroni <giovanni.buroni@inesctec.pt>
2024-10-04 14:06:05 +0100

    add commented future code for data feat engineering

--- source/ensemble/stack_generalization/feature_engineering/data_augmentation.py
+++ source/ensemble/stack_generalization/feature_engineering/data_augmentation.py
@@ -36,6 +36,54 @@ def create_augmented_dataframe(df, max_lags, forecasters_diversity=False, lagged
     df = df.iloc[max_lags:,:]
     return df
 
+# def create_augmented_dataframe(df, max_lags, forecasters_diversity=False, lagged=False, augmented=False, differenciate=False):
+#     " Create feature engineering dataframe with forecasters diversity, lagged, augmented and differenciate features"
+#     assert isinstance(df, pd.DataFrame), "df should be a DataFrame"
+#     assert isinstance(max_lags, int), "max_lags should be an integer"
+#     assert max_lags > 0, "max_lags should be greater than 0"
+#     shifted_df_ensemble = pd.DataFrame()
+#     if forecasters_diversity:
+#         " Create forecasters diversity features"
+#         forecast_cols = [name for name in df.columns if any(q in name for q in ['q50', 'q10', 'q90'])] # forecasters columns
+#         shifted_df_ensemble["forecasters_std"] = df[forecast_cols].std(axis=1)  # standard deviation among forecasters
+#         shifted_df_ensemble["forecasters_var"] = df[forecast_cols].var(axis=1)  # variance among forecasters
+#         shifted_df_ensemble["forecasters_mean"] = df[forecast_cols].mean(axis=1)  # mean among forecasters
+#         # add lagged features to the forecasters diversity features
+#         for lag in range(1, 2):
+#             shifted_df_ensemble["forecasters_std_t-"+str(lag)] = df[forecast_cols].std(axis=1).shift(lag)
+#             shifted_df_ensemble["forecasters_var_t-"+str(lag)] = df[forecast_cols].var(axis=1).shift(lag)
+#             shifted_df_ensemble["forecasters_mean_t-"+str(lag)] = df[forecast_cols].mean(axis=1).shift(lag)
+#     if lagged:
+#         " Create lagged features"
+#         for lag in range(1, max_lags + 1):
+#             for col in df.columns:
+#                 shifted_df_ensemble[col+'_t-'+str(lag)] = df[col].shift(lag)  # lagged
+#     if augmented:
+#         " Create augmented features"
+#         for col in df.columns:
+#             shifted_df_ensemble[col + "_sqr"] = df[col]**2  # squared
+#             shifted_df_ensemble[col + "_roll_avg"] = df[col].rolling(max_lags).mean()  # rolling mean
+#             shifted_df_ensemble[col + "_roll_avg_sqr"] = df[col].rolling(max_lags).mean()**2  # rolling mean squared
+#             shifted_df_ensemble[col + "_roll_std"] = df[col].rolling(max_lags).std()  # rolling standard deviation
+#             shifted_df_ensemble[col + "_roll_sum"] = df[col].rolling(max_lags).sum()  # rolling sum
+#         # add lagged features to the augmented features
+#         for lag in range(1, 2):
+#             for col in df.columns:
+#                 shifted_df_ensemble[col+'_t-'+str(lag)+'_sqr'] = df[col].shift(lag)**2
+#                 shifted_df_ensemble[col+'_t-'+str(lag)+'_roll_avg'] = df[col].shift(lag).rolling(max_lags).mean()
+#                 shifted_df_ensemble[col+'_t-'+str(lag)+'_roll_avg_sqr'] = df[col].shift(lag).rolling(max_lags).mean()**2
+#                 shifted_df_ensemble[col+'_t-'+str(lag)+'_roll_std'] = df[col].shift(lag).rolling(max_lags).std()
+#                 shifted_df_ensemble[col+'_t-'+str(lag)+'_roll_sum'] = df[col].shift(lag).rolling(max_lags).sum()
+#     if differenciate:
+#         " Create differenciate features"
+#         for col in df.columns:
+#             shifted_df_ensemble[col + "_diff"] = df[col].diff()  # difference
+#             shifted_df_ensemble[col + "_lag-1_diff"] = df[col].shift(1).diff()  # difference on lag-1
+#     " Concatenate the original dataframe with the shifted dataframe"
+#     df = pd.concat([df, shifted_df_ensemble], axis=1)
+#     df = df.iloc[max_lags:,:]
+#     return df
+
 
 def augment_with_quantiles(X_train, X_test, df_train_ensemble, 
                             X_train_quantile10, X_test_quantile10, df_train_ensemble_quantile10,
