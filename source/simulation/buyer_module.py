db450dcb53cd3ecc27b6e1f9dd73fb462f1c2558
giovanni.buroni <giovanni.buroni@inesctec.pt>
2024-10-14 13:43:03 +0100

    add docstring

--- source/simulation/buyer_module.py
+++ source/simulation/buyer_module.py
@@ -1,6 +1,16 @@
 import pandas as pd
 
 def prepare_buyer_data(df_train, df_test, start_prediction_timestamp, end_prediction_timestamp):
+    """ Prepare data for the buyer module
+    Args:
+        df_train: DataFrame with training data
+        df_test: DataFrame with test data
+        start_prediction_timestamp: Start timestamp for the prediction
+        end_prediction_timestamp: End timestamp for the prediction
+    Returns:
+        df_buyer: DataFrame with the buyer data
+        forecast_range: Forecast range for the prediction"""
+    
     # Create DataFrame for train and test data
     df_train_buyer = pd.DataFrame(df_train['measured'])
     df_test_buyer = pd.DataFrame(df_test['measured'])
