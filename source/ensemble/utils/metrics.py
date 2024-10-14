433808e55b1e636421b99b01da7be26a405d09e9
giovanni.buroni <giovanni.buroni@inesctec.pt>
2024-10-14 14:24:26 +0100

    replace 'target' with 'targets'

--- source/ensemble/utils/metrics.py
+++ source/ensemble/utils/metrics.py
@@ -12,7 +12,7 @@ def mae(y_pred, y_targ):
     assert len(y_pred) == len(y_targ), "Length mismatch between predictions and targets"
     return np.mean(np.abs(y_pred - y_targ))
 
-def calculate_rmse(df, pred_col, targ_col='target'):
+def calculate_rmse(df, pred_col, targ_col='targets'):
     " Calculate RMSE Loss."
     assert pred_col in df.columns, "prediction column is missing"
     assert targ_col in df.columns, "target column is missing"
@@ -20,7 +20,7 @@ def calculate_rmse(df, pred_col, targ_col='target'):
     rmse_loss['rmse'] = np.array([rmse(df[pred_col], df[targ_col])])
     return rmse_loss
 
-def calculate_mae(df, pred_col, targ_col='target'):
+def calculate_mae(df, pred_col, targ_col='targets'):
     " Calculate MAE Loss."
     assert pred_col in df.columns, "prediction column is missing"
     assert targ_col in df.columns, "target column is missing"
@@ -28,9 +28,9 @@ def calculate_mae(df, pred_col, targ_col='target'):
     mae_loss['mae'] = np.array([mae(df[pred_col], df[targ_col])])
     return mae_loss
 
-def calculate_pinball_losses(df, confidence_10_col, confidence_90_col, targ_col='target'):
+def calculate_pinball_losses(df, confidence_10_col, confidence_90_col, targ_col='targets'):
     " Calculate Pinball Losses for 10% and 90% quantiles."
-    assert 'target' in df.columns, "target column is missing"
+    assert 'targets' in df.columns, "target column is missing"
     pinball_losses = pd.DataFrame()
     score_10 = mean_pinball_loss( list(df[targ_col].values),  list(df[confidence_10_col].values), alpha=0.1)
     score_90 = mean_pinball_loss( list(df[targ_col].values),  list(df[confidence_90_col].values), alpha=0.9)
