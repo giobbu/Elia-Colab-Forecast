e31816fc45291199761d92ac6fce33bf4b9f3dcc
giovanni.buroni <giovanni.buroni@inesctec.pt>
2024-10-14 14:07:57 +0100

    refactor plt forecast function

--- source/plots/plot_forecasts.py
+++ source/plots/plot_forecasts.py
@@ -8,22 +8,32 @@ def plot_elia_forecasts(df, which = 'norm_dayahead'):
     df[list_cols].plot(color='blue', linestyle='--')
     df['diff_norm_measured'].plot(color='red')
 
-def plot_ensemble_forecasts(df_pred_ensemble, df_ensemble):
+def plot_forecasts(df_pred, df_target, list_wind_ramps, title, color='blue'):
     " Plot ensemble forecasts "
-    assert 'target' in list(df_ensemble), 'target not in df_ensemble'
+    assert 'targets' in list(df_target), 'targets not in df_target'
     # Create a figure and a set of subplots
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(15, 7))
     # Plot '10_predictions' and '90_predictions' on the same axes with blue dashed lines
-    df_plot = df_pred_ensemble[['10_predictions', '90_predictions']]
+    df_plot = df_pred[['10_predictions', '90_predictions']]
     df_plot.columns = ['Q10', 'Q90']
-    df_plot.plot(ax=ax, color='blue', linestyle='--')
+    ax.plot(df_plot.index, df_plot['Q10'], color='blue', linestyle='--', alpha=0.1, label='Q10')
+    ax.plot(df_plot.index, df_plot['Q90'], color='blue', linestyle='--', alpha=0.1, label='Q90')
     # Plot '50_predictions' on the same axes with a solid blue line
-    df_plot_mean = df_pred_ensemble[['50_predictions']]
+    df_plot_mean = df_pred[['50_predictions']]
     df_plot_mean.columns = ['MEAN']
-    df_plot_mean.plot(ax=ax, color='blue')
-    df_target = df_ensemble[['target']]
-    df_target.columns = ['target']
-    df_target.plot(ax=ax, color='red')
+    ax.plot(df_plot_mean.index, df_plot_mean['MEAN'], color='blue', alpha=0.5, label='Mean')
+    ax.plot(df_target.index, df_target['targets'], color='red', label='Target')
+    # fill area between Q10 and Q90
+    ax.fill_between(df_plot.index, df_plot['Q10'], df_plot['Q90'], color=color, alpha=0.05, label='80% prediction interval')
+    if len(list_wind_ramps) != 0:
+        for i, ramp in enumerate(list_wind_ramps):
+            ax.axvline(ramp, color='black', alpha=0.75, label=f'Wind Ramp {i}')
+    ax.grid(True)
+    ax.legend()
+    plt.title(title)
+    plt.show()
+
+
 
 def plot_var_ensemble_forecasts(df_pred_ensemble, df_ensemble):
     " Plot ensemble forecasts "
