be1ad395807e45a48cc6108a7bfabe13282a54ab
giovanni.buroni <giovanni.buroni@inesctec.pt>
2024-10-17 17:46:22 +0100

    refactor col_permutation to not repeat combination sets of features

--- source/ensemble/stack_generalization/test_importance/first_stage_importance_shap.py
+++ source/ensemble/stack_generalization/test_importance/first_stage_importance_shap.py
@@ -93,13 +93,32 @@ def compute_row_perm_score(seed, fitted_model, set_feat2permutate, predictor_ind
     # return the difference in error
     return max(0, score_with_permutation - score_without_permutation)
 
-def compute_col_perm_score(seed, params_model, nr_features, X_test_augm, y_test, fitted_model, score_function, predictor_index):
-    " Compute  score for a single predictor."
+
+def compute_col_perm_score(seed, params_model, nr_features, X_test_augm, y_test, fitted_model, score_function, predictor_index, list_set_feat2permutate):
+    """Compute score for a single predictor."""
+    # Define the maximum number of iterations
+    max_iterations = 2 * nr_features - 1
+    iteration_count = 0
+    # 1) Get the column permutation
     col_perm = run_col_permutation(seed, nr_features)
+    # 2) Ensure that the first element of col_perm is not the predictor_index
     while col_perm[0] == predictor_index:
-        seed = seed + 1
+        seed += 1
         col_perm = run_col_permutation(seed, nr_features)
+    # 3) Get the set of features to permute
     set_feat2permutate = col_perm[np.arange(0, np.where(col_perm == predictor_index)[0][0])]
+    # transform set_feat2permutate to an unique string
+    str_set_feat2permutate = ''.join(str(e) for e in set_feat2permutate)
+    # 4) if the set of features to permute has already been computed and iteration is lower than max_iterations, 
+    # find a new set of features to permute
+    while str_set_feat2permutate in list_set_feat2permutate and iteration_count < max_iterations:
+        iteration_count += 1
+        seed += 1  # Increment seed to get a different permutation
+        col_perm = run_col_permutation(seed, nr_features)
+        set_feat2permutate = col_perm[np.arange(0, np.where(col_perm == predictor_index)[0][0])]
+        # transform set_feat2permutate to an unique string
+        str_set_feat2permutate = ''.join(str(e) for e in set_feat2permutate)
+    # Permute features in the test set
     X_test_perm_with, X_test_perm_without = X_test_augm.copy(), X_test_augm.copy()
     # Compute row scores using parallel processing
     row_scores = Parallel(n_jobs=4)(
@@ -110,7 +129,7 @@ def compute_col_perm_score(seed, params_model, nr_features, X_test_augm, y_test,
     )
     # Compute the final column score
     col_score = np.mean(row_scores)
-    return col_score
+    return col_score, str_set_feat2permutate
 
 def first_stage_shapley_importance(y_test, params_model, quantile, info_previous_day_first_stage):
     " Compute permutation importances for the first stage model."
