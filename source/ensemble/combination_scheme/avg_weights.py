d7a22a8bde6f980e48e63f6156ecb8a4cfb45d13
giovanni.buroni <giovanni.buroni@inesctec.pt>
2024-10-18 10:57:21 +0100

    add docstrings in compute_weight

--- source/ensemble/combination_scheme/avg_weights.py
+++ source/ensemble/combination_scheme/avg_weights.py
@@ -3,7 +3,12 @@ import pandas as pd
 from sklearn.metrics import mean_pinball_loss
 
 def compute_weight(loss, norm):
-    " Compute the weight based on the pinball loss of the forecasts"
+    """ Compute the weight based on the pinball loss of the forecasts
+    args:
+        loss: float, pinball loss
+        norm: str, normalization method
+    returns:
+        weight: float, weight"""
     if norm=='sum':
         weight = 1/loss
     elif norm=='softmax':
