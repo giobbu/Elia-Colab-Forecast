c11475e1db4e8d2a599772df1f6d6446b355cca0
giovanni.buroni <giovanni.buroni@inesctec.pt>
2024-10-18 07:59:01 +0100

    move functions in optimization module

--- source/ensemble/stack_generalization/hyperparam_optimization/optimization.py
+++ source/ensemble/stack_generalization/hyperparam_optimization/optimization.py
@@ -1,9 +1,11 @@
+import numpy as np
+from loguru import logger
+from sklearn.ensemble import HistGradientBoostingRegressor
+from sklearn.linear_model import QuantileRegressor, Lasso
+
 from source.ensemble.stack_generalization.hyperparam_optimization.models.quantile_gbr import optimize_gbr
 from source.ensemble.stack_generalization.hyperparam_optimization.models.quantile_lr import optimize_lr
 
-from sklearn.ensemble import HistGradientBoostingRegressor
-from sklearn.linear_model import QuantileRegressor, Lasso
-from loguru import logger
 
 def optimize_model(X_train, y_train, quantile, nr_cv_splits, model_type, solver, gbr_config_params, lr_config_params):
     " Optimize selected model hyperparameters."
@@ -33,3 +35,53 @@ def initialize_model(model_type, quantile, best_params, solver):
     else:
         raise ValueError('"model_type" is not valid')
     return model
+
+def initialize_train_and_predict(predictions, 
+                                model_type, 
+                                quantile, 
+                                best_params, 
+                                solver, 
+                                X_train, 
+                                y_train, 
+                                X_test, 
+                                insample=False, 
+                                predictions_insample=None, 
+                                predictions_outsample=None):
+    """
+    Initializes, fits a model, and makes predictions.
+    """
+    # Initialize model with best params
+    model = initialize_model(model_type, quantile, best_params, solver)
+    # Fit model
+    fitted_model = model.fit(X_train, y_train)
+    # Make predictions
+    predictions[quantile] = fitted_model.predict(X_test)
+    # Return fitted model and predictions
+    if not insample:
+        return fitted_model, predictions
+    elif insample:
+        predictions_insample[quantile] = fitted_model.predict(X_train)
+        predictions_outsample[quantile] = fitted_model.predict(X_test) 
+        return fitted_model, predictions, predictions_insample, predictions_outsample
+
+def permutation_quantile_regression(best_params, solver, X, y, quantile, n_permutations=100):
+    " Perform permutation-based quantile regression to calculate p-values."
+    coefs = []
+    # Fit the model on the original dataset to get the observed coefficients
+    model_original = QuantileRegressor(quantile=quantile, solver=solver, **best_params).fit(X, y)
+    coefs_original = model_original.coef_
+    for _ in range(n_permutations):
+        # Permute y (random shuffle)
+        y_permuted = np.random.permutation(y)
+        if quantile == 0.5:
+            # Fit the model on the permuted dataset
+            model = Lasso(**best_params).fit(X, y_permuted)
+        else:
+            # Fit the model on the permuted dataset
+            model = QuantileRegressor(quantile=quantile, solver=solver, **best_params).fit(X, y_permuted)
+        coefs.append(model.coef_)
+    # Convert the list of coefficients to a NumPy array
+    coefs = np.array(coefs)
+    # Calculate p-values by comparing original coefficients to the permuted coefficients
+    p_values = np.mean(np.abs(coefs) >= np.abs(coefs_original), axis=0)
+    return coefs_original, p_values
