cf32a2007542e55e2af8bcc889732dec31ba5219
giovanni.buroni <giovanni.buroni@inesctec.pt>
2024-12-01 10:40:09 +0000

    refactor and add feature feature engineering

--- source/ensemble/stack_generalization/feature_engineering/data_augmentation.py
+++ source/ensemble/stack_generalization/feature_engineering/data_augmentation.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import numpy as np
+from loguru import logger
 
-def create_augmented_dataframe(df, max_lags, forecasters_diversity=False, lagged=False, augmented=False, differenciate=False):
+def create_augmented_dataframe(df, max_lags, forecasters_diversity=False, add_lags=False, 
+                                augment_with_poly=False, augment_with_roll_stats=False, differenciate=False,
+                                end_train=None, start_prediction=None):
     """ Create feature engineering dataframe with forecasters diversity, lagged, augmented and differenciate features 
     args:
         df: pd.DataFrame, dataframe
@@ -11,87 +14,70 @@ def create_augmented_dataframe(df, max_lags, forecasters_diversity=False, lagged
         augmented: bool, create augmented features
         differenciate: bool, create differenciate features
     returns:
-        df: pd.DataFrame, dataframe with the new features"""
+        df: pd.DataFrame, dataframe with the new features
+    """
     assert isinstance(df, pd.DataFrame), "df should be a DataFrame"
     assert isinstance(max_lags, int), "max_lags should be an integer"
-    assert max_lags > 0, "max_lags should be greater than 0"
+    if add_lags:
+        assert max_lags > 0, "max_lags should be greater than 0"
+    else:
+        assert max_lags == 0, "max_lags should be 0 when lagged is False"
     shifted_df_ensemble = pd.DataFrame()
     if forecasters_diversity:
         " Create forecasters diversity features"
         forecast_cols = [name for name in df.columns if any(q in name for q in ['q50', 'q10', 'q90'])] # forecasters columns
         shifted_df_ensemble["forecasters_std"] = df[forecast_cols].std(axis=1)  # standard deviation among forecasters
         shifted_df_ensemble["forecasters_var"] = df[forecast_cols].var(axis=1)  # variance among forecasters
-    if lagged:
+        shifted_df_ensemble["forecasters_mean"] = df[forecast_cols].mean(axis=1)  # mean among forecasters
+        shifted_df_ensemble["forecasters_prod"] = df[forecast_cols].prod(axis=1)  # product among forecasters
+    if add_lags:
         " Create lagged features"
-        for lag in range(1, max_lags + 1):
-            for col in df.columns:
-                shifted_df_ensemble[col+'_t-'+str(lag)] = df[col].shift(lag)  # lagged
-    if augmented:
+        for col in df.columns:
+            if max_lags > 1:
+                for lag in range(1, max_lags + 1):
+                        shifted_df_ensemble[col+'_t-'+str(lag)] = df[col].shift(lag)  # lagged
+            else:
+                shifted_df_ensemble[col+'_t-1'] = df[col].shift(1)
+    if augment_with_poly:
         " Create augmented features"
         for col in df.columns:
             shifted_df_ensemble[col + "_sqr"] = df[col]**2  # squared
-            shifted_df_ensemble[col + "_std"] = df[col].rolling(max_lags).std()  # rolling standard deviation
-            shifted_df_ensemble[col + "_var"] = df[col].rolling(max_lags).var()  # rolling variance
-            if max_lags > 2:
-                shifted_df_ensemble[col + "_lag-1_std"] = df[col].shift(1).rolling(max_lags-1).std()  # rolling standard deviation on lag-1
-                shifted_df_ensemble[col + "_lag-1_var"] = df[col].shift(1).rolling(max_lags-1).var()  # rolling variance on lag-1
+            shifted_df_ensemble[col + "_cub"] = df[col]**3  # cubic
+    if augment_with_roll_stats:
+        if add_lags:
+            for col in df.columns:
+                shifted_df_ensemble[col + "_avg"] = df[col].rolling(max_lags).mean()  # rolling average
+                if max_lags > 1:
+                    shifted_df_ensemble[col + "_std"] = df[col].rolling(max_lags).std()  # rolling standard deviation
+                    shifted_df_ensemble[col + "_var"] = df[col].rolling(max_lags).var()  # rolling variance
+                if max_lags > 2:
+                    shifted_df_ensemble[col + "_lag-1_avg"] = df[col].shift(1).rolling(max_lags-1).mean()  # rolling average on lag-1
+                    shifted_df_ensemble[col + "_lag-1_std"] = df[col].shift(1).rolling(max_lags-1).std()  # rolling standard deviation on lag-1
+                    shifted_df_ensemble[col + "_lag-1_var"] = df[col].shift(1).rolling(max_lags-1).var()  # rolling variance on lag-1
     if differenciate:
         " Create differenciate features"
-        for col in df.columns:
-            shifted_df_ensemble[col + "_diff"] = df[col].diff()  # difference
-            shifted_df_ensemble[col + "_lag-1_diff"] = df[col].shift(1).diff()  # difference on lag-1
+        if add_lags:
+            for col in df.columns:
+                shifted_df_ensemble[col + "_diff"] = df[col].diff()  # difference
+                shifted_df_ensemble[col + "_lag-1_diff"] = df[col].shift(1).diff()  # difference on lag-1
     " Concatenate the original dataframe with the shifted dataframe"
-    df = pd.concat([df, shifted_df_ensemble], axis=1)
-    df = df.iloc[max_lags:,:]
-    return df
 
-# def create_augmented_dataframe(df, max_lags, forecasters_diversity=False, lagged=False, augmented=False, differenciate=False):
-#     " Create feature engineering dataframe with forecasters diversity, lagged, augmented and differenciate features"
-#     assert isinstance(df, pd.DataFrame), "df should be a DataFrame"
-#     assert isinstance(max_lags, int), "max_lags should be an integer"
-#     assert max_lags > 0, "max_lags should be greater than 0"
-#     shifted_df_ensemble = pd.DataFrame()
-#     if forecasters_diversity:
-#         " Create forecasters diversity features"
-#         forecast_cols = [name for name in df.columns if any(q in name for q in ['q50', 'q10', 'q90'])] # forecasters columns
-#         shifted_df_ensemble["forecasters_std"] = df[forecast_cols].std(axis=1)  # standard deviation among forecasters
-#         shifted_df_ensemble["forecasters_var"] = df[forecast_cols].var(axis=1)  # variance among forecasters
-#         shifted_df_ensemble["forecasters_mean"] = df[forecast_cols].mean(axis=1)  # mean among forecasters
-#         # add lagged features to the forecasters diversity features
-#         for lag in range(1, 2):
-#             shifted_df_ensemble["forecasters_std_t-"+str(lag)] = df[forecast_cols].std(axis=1).shift(lag)
-#             shifted_df_ensemble["forecasters_var_t-"+str(lag)] = df[forecast_cols].var(axis=1).shift(lag)
-#             shifted_df_ensemble["forecasters_mean_t-"+str(lag)] = df[forecast_cols].mean(axis=1).shift(lag)
-#     if lagged:
-#         " Create lagged features"
-#         for lag in range(1, max_lags + 1):
-#             for col in df.columns:
-#                 shifted_df_ensemble[col+'_t-'+str(lag)] = df[col].shift(lag)  # lagged
-#     if augmented:
-#         " Create augmented features"
-#         for col in df.columns:
-#             shifted_df_ensemble[col + "_sqr"] = df[col]**2  # squared
-#             shifted_df_ensemble[col + "_roll_avg"] = df[col].rolling(max_lags).mean()  # rolling mean
-#             shifted_df_ensemble[col + "_roll_avg_sqr"] = df[col].rolling(max_lags).mean()**2  # rolling mean squared
-#             shifted_df_ensemble[col + "_roll_std"] = df[col].rolling(max_lags).std()  # rolling standard deviation
-#             shifted_df_ensemble[col + "_roll_sum"] = df[col].rolling(max_lags).sum()  # rolling sum
-#         # add lagged features to the augmented features
-#         for lag in range(1, 2):
-#             for col in df.columns:
-#                 shifted_df_ensemble[col+'_t-'+str(lag)+'_sqr'] = df[col].shift(lag)**2
-#                 shifted_df_ensemble[col+'_t-'+str(lag)+'_roll_avg'] = df[col].shift(lag).rolling(max_lags).mean()
-#                 shifted_df_ensemble[col+'_t-'+str(lag)+'_roll_avg_sqr'] = df[col].shift(lag).rolling(max_lags).mean()**2
-#                 shifted_df_ensemble[col+'_t-'+str(lag)+'_roll_std'] = df[col].shift(lag).rolling(max_lags).std()
-#                 shifted_df_ensemble[col+'_t-'+str(lag)+'_roll_sum'] = df[col].shift(lag).rolling(max_lags).sum()
-#     if differenciate:
-#         " Create differenciate features"
-#         for col in df.columns:
-#             shifted_df_ensemble[col + "_diff"] = df[col].diff()  # difference
-#             shifted_df_ensemble[col + "_lag-1_diff"] = df[col].shift(1).diff()  # difference on lag-1
-#     " Concatenate the original dataframe with the shifted dataframe"
-#     df = pd.concat([df, shifted_df_ensemble], axis=1)
-#     df = df.iloc[max_lags:,:]
-#     return df
+    df = pd.concat([df, shifted_df_ensemble], axis=1)
+    if add_lags:
+        df = df.iloc[max_lags:, :]  # remove the first rows with NaN values
+        df_train = df[df.index < end_train]
+        df_test = df[df.index >= start_prediction]
+        for lag in range(1, max_lags + 1):
+            lag_colunms = df_train.columns.str.contains('_t-'+str(lag))
+            # set the first lag rows to nans
+            df_train.loc[:df_train.index[lag - 1],df_train.columns[lag_colunms]] = np.nan
+            df_test.loc[:df_test.index[lag - 1], df_test.columns[lag_colunms]] = np.nan
+            # backfill the nans
+            df_train.bfill(inplace=True)
+            df_test.bfill(inplace=True)
+            # concatenate the training and testing data
+            df = pd.concat([df_train, df_test], axis=0)
+    return df
 
 
 def augment_with_quantiles(X_train, X_test, df_train_ensemble, 
