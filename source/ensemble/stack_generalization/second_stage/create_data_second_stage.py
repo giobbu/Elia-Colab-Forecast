e5b7f225d85e5a182f9887e8b1ea6bf752166c25
giovanni.buroni <giovanni.buroni@inesctec.pt>
2024-12-01 10:44:47 +0000

    add refactored code for feat eng second stage

--- source/ensemble/stack_generalization/second_stage/create_data_second_stage.py
+++ source/ensemble/stack_generalization/second_stage/create_data_second_stage.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 def create_2stage_dataframe(df_train_ensemble, df_test_ensemble, y_train, y_test, predictions_insample, predictions_outsample):
     " Create 2-stage ensemble dataframe."
@@ -24,42 +25,53 @@ def create_2stage_dataframe(df_train_ensemble, df_test_ensemble, y_train, y_test
     df_2stage = pd.concat([df_insample, df_outsample], axis=0)
     return df_2stage
 
-def create_augmented_dataframe_2stage(df_2stage, order_diff, max_lags, augment=False):
+def create_augmented_dataframe_2stage(df, order_diff, max_lags, differentiate = False, add_lags=False, augment_with_poly=False, end_train=None, start_prediction=None):
     " Process 2-stage ensemble dataframe with lags."
     assert order_diff > 0, "Order of differentiation must be greater than 0"
-    assert max_lags > 0, "Maximum number of lags must be greater than 0"
+    if add_lags:
+        assert max_lags > 0, "max_lags should be greater than 0"
+    else:
+        assert max_lags == 0, "max_lags should be 0 when lagged is False"
+    # Differentiate the targets
+    for col in df.columns:
+        if 'targets' in col:
+            df[col] = df[col].diff(order_diff)
     # Differentiate the dataframe
-    df_2stage_diff = df_2stage.diff(order_diff)
-    # Create lagged features
-    for lag in range(1, max_lags + 1):
-        df_2stage_diff[f'predictions_t-{lag}'] = df_2stage_diff['predictions'].shift(lag)
-    if augment:
-        for col in df_2stage_diff.columns:
+    if differentiate:
+        for col in df.columns:
             if 'targets' not in col:
-                df_2stage_diff[f'{col}_sqr'] = df_2stage_diff[col]**2
+                df[f'{col}_diff'] = df[col].diff(order_diff)
+    # Create lagged features
+    if add_lags:
+        for col in df.columns:
+            for lag in range(1, max_lags + 1):
+                if 'targets' not in col:
+                    df[col+'_t-'+str(lag)] = df[col].shift(lag)
+    if augment_with_poly:
+        for col in df.columns:
+            if 'targets' not in col:
+                df[f'{col}_sqr'] = df[col]**2
+                df[f'{col}_cub'] = df[col]**3
     # Drop rows with NaNs resulting from the shift operation
-    df_2stage_process = df_2stage_diff.iloc[max_lags+order_diff:, :]
-    return df_2stage_process
-
-# def create_augmented_dataframe_2stage(df_2stage, order_diff, max_lags, augment=False):
-#     " Process 2-stage ensemble dataframe with lags."
-#     assert order_diff > 0, "Order of differentiation must be greater than 0"
-#     assert max_lags > 0, "Maximum number of lags must be greater than 0"
-#     # Differentiate the dataframe
-#     df_2stage_diff = df_2stage.diff(order_diff)
-#     # Create lagged features
-#     for lag in range(1, max_lags + 1):
-#         df_2stage_diff[f'predictions_t-{lag}'] = df_2stage_diff['predictions'].shift(lag)
-#         df_2stage_diff[f'predictions_t-{lag}_sqr'] = df_2stage_diff['predictions'].shift(lag)**2
-#         df_2stage_diff[f'predictions_t-{lag}_cubic'] = df_2stage_diff['predictions'].shift(lag)**3
-#     if augment:
-#         for col in df_2stage_diff.columns:
-#             if 'targets' not in col:
-#                 df_2stage_diff[f'{col}_sqr'] = df_2stage_diff[col]**2
-#                 df_2stage_diff[f'{col}_cubic'] = df_2stage_diff[col]**3
-#     # Drop rows with NaNs resulting from the shift operation
-#     df_2stage_process = df_2stage_diff.iloc[max_lags+order_diff:, :]
-#     return df_2stage_process
+    if add_lags:
+        cut_ = max_lags + order_diff
+    else:
+        cut_ = order_diff
+    df_ = df.iloc[cut_:, :]
+    if add_lags:
+        df_train = df_[df_.index < end_train]
+        df_test = df_[df_.index >= start_prediction]
+        for lag in range(1, max_lags + 1):
+            lag_colunms = df_train.columns.str.contains('_t-'+str(lag))
+            # set the first lag rows to nans
+            df_train.loc[:df_train.index[lag - 1],df_train.columns[lag_colunms]] = np.nan
+            df_test.loc[:df_test.index[lag - 1], df_test.columns[lag_colunms]] = np.nan
+            # backfill the nans
+            df_train = df_train.bfill()
+            df_test = df_test.bfill()
+            # concatenate the training and testing data
+            df_ = pd.concat([df_train, df_test])
+    return df_
 
 def create_var_ensemble_dataframe(buyer_resource_name, quantiles, quantile_predictions_dict, df_test):
     " Create ensemble dataframe from quantile predictions."
@@ -76,7 +88,6 @@ def create_var_ensemble_dataframe(buyer_resource_name, quantiles, quantile_predi
             df_pred_quantile.set_index('datetime', inplace=True)
             df_pred_ensemble = pd.concat([df_pred_ensemble, df_pred_quantile], axis=1)
     return df_pred_ensemble
-
 
 
 def get_numpy_Xy_train_test_2stage(df_2stage_train, df_2stage_test):
