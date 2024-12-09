bbf6ca56b9e7d9241ca09afbd96bca9c0361e73c
giovanni.buroni <giovanni.buroni@inesctec.pt>
2024-12-09 14:52:28 +0000

    move logs for timestamps

--- source/utils/generate_timestamp.py
+++ source/utils/generate_timestamp.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from loguru import logger
 
 def generate_timestamps(start_training, i, window_size):
     # Generate timestamps for training and prediction
@@ -7,4 +8,12 @@ def generate_timestamps(start_training, i, window_size):
     end_training_timestamp = pd.to_datetime(start_training, utc=True) + pd.Timedelta(days=i + window_size)
     start_prediction_timestamp = pd.to_datetime(start_training, utc=True) + pd.Timedelta(days=1 + i + window_size)
     end_prediction_timestamp = pd.to_datetime(start_training, utc=True) + pd.Timedelta(days=2 + i + window_size)
+
+    logger.info(' ')
+    logger.opt(colors = True).info('<blue>-------------------------------------------------------------------------------------------</blue>')
+    logger.opt(colors=True).info(f'<blue>Start training: {start_training_timestamp} - End training: {end_training_timestamp}</blue>')
+    logger.opt(colors = True).info('<blue>-------------------------------------------------------------------------------------------</blue>')
+    logger.opt(colors = True).info(f'<blue>Start prediction: {start_prediction_timestamp} - End prediction: {end_prediction_timestamp}</blue>')
+
     return start_training_timestamp, end_training_timestamp, start_prediction_timestamp, end_prediction_timestamp
+
