ca14946282b6adbc0a81bf83648b6e095a22bfbc
giovanni.buroni <giovanni.buroni@inesctec.pt>
2024-12-09 15:21:34 +0000

    rename cols equal_weights scheme

--- source/ensemble/combination_scheme/equal_weights.py
+++ source/ensemble/combination_scheme/equal_weights.py
@@ -13,14 +13,14 @@ def calculate_equal_weights(df_test_norm, start_prediction_timestamp):
     list_q10 = [name for name in list(df_test_norm.columns) if 'confidence10' in name]  # list of columns with q10
     list_q50 = [name for name in list(df_test_norm.columns) if 'forecast' in name]  # list of columns with q50
     list_q90 = [name for name in list(df_test_norm.columns) if 'confidence90' in name]  # list of columns with q90
-    Q10 = df_test_norm[list_q10].mean(axis=1)  # mean of q10
-    MEAN = df_test_norm[list_q50].mean(axis=1)  # mean of q50
-    Q90 = df_test_norm[list_q90].mean(axis=1)  # mean of q90
+    q10_equal_weights = df_test_norm[list_q10].mean(axis=1)  # mean of q10
+    q50_equal_weights = df_test_norm[list_q50].mean(axis=1)  # mean of q50
+    q90_equal_weights = df_test_norm[list_q90].mean(axis=1)  # mean of q90
     # Create a DataFrame with the mean prediction and quantiles
     df_equal_weights = pd.DataFrame({
-        'Q10': Q10,
-        'mean_prediction': MEAN,
-        'Q90': Q90
+        'q10_equal_weights': q10_equal_weights,
+        'q50_equal_weights': q50_equal_weights,
+        'q90_equal_weights': q90_equal_weights
     }, index=df_test_norm.index)
     # Add the target column
     df_equal_weights['targets'] = df_test_norm['norm_measured']
