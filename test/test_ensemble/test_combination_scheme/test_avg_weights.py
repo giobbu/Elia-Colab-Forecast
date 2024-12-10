33754387a5ef70070e29a5a75c1beec82d9eab27
giovanni.buroni <giovanni.buroni@inesctec.pt>
2024-12-10 13:13:57 +0000

    update pytest init

--- test/test_ensemble/test_combination_scheme/test_avg_weights.py
+++ test/test_ensemble/test_combination_scheme/test_avg_weights.py
@@ -1,30 +1,8 @@
 import pytest
 import pandas as pd
 import numpy as np
-from source.ensemble.combination_scheme.avg_weights import calculate_weights, normalize_weights, calculate_combination_forecast, create_weighted_avg_df, calculate_weighted_avg
+from source.ensemble.combination_scheme.avg_weights import normalize_weights, calculate_combination_forecast
 
-def test_calculate_weights_empty_dataframe():
-    "Test calculate_weights with empty dataframe"
-    df = pd.DataFrame()
-    with pytest.raises(AssertionError, match='Dataframe is empty'):
-        calculate_weights(df)
-
-def test_calculate_weights_valid_dataframe(data):
-    "Test calculate_weights with valid dataframe"
-    df = pd.DataFrame(data)
-    lst_cols_forecasts, lst_q10_weight, lst_q50_weight, lst_q90_weight = calculate_weights(df)
-    assert 'forecast' in lst_cols_forecasts
-    assert 'confidence10' in lst_cols_forecasts
-    assert 'confidence90' in lst_cols_forecasts
-    assert lst_q10_weight[0]['confidence10'] > 0
-    assert lst_q50_weight[0]['forecast'] > 0
-    assert lst_q90_weight[0]['confidence90'] > 0
-
-def test_calculate_weights_invalid_column(invalid_data):
-    "Test calculate_weights with invalid column"
-    df = invalid_data
-    with pytest.raises(ValueError, match='Not a valid column'):
-        calculate_weights(df)
 
 def test_normalize_weights_single_item(single_weight):
     "Test normalize_weights with single item"
@@ -65,45 +43,3 @@ def test_calculate_combination_forecast_forecasts_confidence10_confidence90(mock
     np.testing.assert_array_almost_equal(combination_forecast, expected_combination_forecast)
     np.testing.assert_array_almost_equal(combination_quantile10, expected_combination_quantile10)
     np.testing.assert_array_almost_equal(combination_quantile90, expected_combination_quantile90)
-
-
-def test_create_weighted_avg_df(mock_data_for_weighted_avg, mock_combination_forecast, mock_combination_quantile10, mock_combination_quantile90):
-    "Test create_weighted_avg_df with valid data"
-    df = mock_data_for_weighted_avg
-    combination_forecast = mock_combination_forecast
-    combination_quantile10 = mock_combination_quantile10
-    combination_quantile90 = mock_combination_quantile90
-    result_df = create_weighted_avg_df(df, combination_forecast, combination_quantile10, combination_quantile90)
-    expected_df = pd.DataFrame({
-        'Q10': combination_quantile10,
-        'mean_prediction': combination_forecast,
-        'Q90': combination_quantile90,
-        'diff_norm_measured': df['diff_norm_measured']
-    }, index=df.index)
-    pd.testing.assert_frame_equal(result_df, expected_df)
-
-
-def test_calculate_weighted_avg(mock_calculate_weighted_avg_data):
-
-    df_train_norm_diff, df_test_norm_diff, start_predictions = mock_calculate_weighted_avg_data
-
-    print(df_test_norm_diff)
-
-    # Test without var
-    df_weighted_avg, dict_weights = calculate_weighted_avg(df_train_norm_diff, df_test_norm_diff, start_predictions, window_size_valid=1, var=False)
-    
-    assert 'mean_prediction' in df_weighted_avg.columns
-    assert 'Q10' in df_weighted_avg.columns
-    assert 'Q90' in df_weighted_avg.columns
-    assert 'diff_norm_measured' in df_weighted_avg.columns
-    assert 0.5 in dict_weights
-    assert 0.1 in dict_weights
-    assert 0.9 in dict_weights
-    
-    # Test with var
-    df_weighted_avg_var, dict_weights_var = calculate_weighted_avg(df_train_norm_diff, df_test_norm_diff, start_predictions, window_size_valid=1, var=True)
-    
-    assert 'mean_prediction' in df_weighted_avg_var.columns
-    assert 'diff_norm_measured' in df_weighted_avg_var.columns
-    assert 0.5 in dict_weights_var
-
--- test/test_ensemble/test_stack_generalization/test_data_train_test.py
+++ test/test_ensemble/test_stack_generalization/test_data_train_test.py
@@ -1,52 +1,8 @@
 import pytest
 import pandas as pd
 import numpy as np
-from source.ensemble.stack_generalization.data_preparation.data_train_test import prepare_train_test_data, get_numpy_Xy_train_test
+from source.ensemble.stack_generalization.data_preparation.data_train_test import get_numpy_Xy_train_test
 
-def test_prepare_train_test_data_parameter_types(mock_train_test_split_data):
-    "Test if the function raises AssertionError for invalid parameter types"
-    df_ensemble, df_val, df_test, start_predictions, max_lag = mock_train_test_split_data
-    # Modify types to provoke assertion errors
-    with pytest.raises(AssertionError, match="df_ensemble should be a DataFrame"):
-        prepare_train_test_data(None, df_val, df_test, start_predictions, max_lag)
-    with pytest.raises(AssertionError, match="df_val should be a DataFrame"):
-        prepare_train_test_data(df_ensemble, None, df_test, start_predictions, max_lag)
-    with pytest.raises(AssertionError, match="df_test should be a DataFrame"):
-        prepare_train_test_data(df_ensemble, df_val, None, start_predictions, max_lag)
-    with pytest.raises(AssertionError, match="start_predictions should be a Timestamp"):
-        prepare_train_test_data(df_ensemble, df_val, df_test, "2024-01-06", max_lag)
-    with pytest.raises(AssertionError, match="max_lag should be an integer"):
-        prepare_train_test_data(df_ensemble, df_val, df_test, start_predictions, "2")
-    with pytest.raises(AssertionError, match="max_lag should be greater than 0"):
-        prepare_train_test_data(df_ensemble, df_val, df_test, start_predictions, 0)
-
-def test_prepare_train_test_data_missing_column(mock_train_test_split_data):
-    "Test if the function raises AssertionError if 'diff_norm_measured' column is missing"
-    df_ensemble, df_val, df_test, start_predictions, max_lag = mock_train_test_split_data
-    # Remove 'diff_norm_measured' from df_val
-    df_val_without_column = df_val.drop(columns=['diff_norm_measured'])
-    # Check if assertion error is raised
-    with pytest.raises(AssertionError, match="diff_norm_measured should be in df_val columns"):
-        prepare_train_test_data(df_ensemble, df_val_without_column, df_test, start_predictions, max_lag)
-    # Remove 'diff_norm_measured' from df_test
-    df_test_without_column = df_test.drop(columns=['diff_norm_measured'])
-    # Check if assertion error is raised
-    with pytest.raises(AssertionError, match="diff_norm_measured should be in df_test columns"):
-        prepare_train_test_data(df_ensemble, df_val, df_test_without_column, start_predictions, max_lag)
-
-def test_prepare_train_test_data(mock_train_test_split_data):
-    "Test prepare_train_test_data with valid data"
-    df_ensemble, df_val, df_test, start_predictions, max_lag = mock_train_test_split_data
-    df_train_ensemble, df_test_ensemble = prepare_train_test_data(df_ensemble, df_val, df_test, start_predictions, max_lag)
-    # Check the type of the result
-    assert isinstance(df_train_ensemble, pd.DataFrame)
-    assert isinstance(df_test_ensemble, pd.DataFrame)
-    # Check the content of the result
-    assert 'diff_norm_targ' in df_train_ensemble.columns
-    assert 'diff_norm_targ' in df_test_ensemble.columns
-    # Check the values
-    assert all(df_train_ensemble['diff_norm_targ'].values == df_val['diff_norm_measured'].values[max_lag:])
-    assert all(df_test_ensemble['diff_norm_targ'].values == df_test['diff_norm_measured'].values)
 
 def test_get_numpy_Xy_train_test(mock_data_for_get_numpy_Xy_train_test):
     "Test get_numpy_Xy_train_test with valid data"
