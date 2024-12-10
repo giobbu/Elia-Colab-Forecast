ccfbcf652bd957973de3a489835b9b523c7b1542
giovanni.buroni <giovanni.buroni@inesctec.pt>
2024-12-10 16:50:52 +0000

    add pytest for equal_weight_scheme

--- test/test_ensemble/test_combination_scheme/test_equal_weights.py
+++ test/test_ensemble/test_combination_scheme/test_equal_weights.py
@@ -2,19 +2,21 @@ import pytest
 import pandas as pd
 from source.ensemble.combination_scheme.equal_weights import calculate_equal_weights
 
+def test_calculate_equal_weights(mock_calculate_equal_weights_data):
+    " Test calculate_equal_weights with valid data"
+    df_test_norm_diff = mock_calculate_equal_weights_data
+    df_equal_weights = calculate_equal_weights(df_test_norm_diff, start_prediction_timestamp='2022-01-01 00:00:00')
+    expected_data = {
+        'q10_equal_weights': [float(i - 1) for i in range(96)],
+        'q50_equal_weights': [float(i) for i in range(96)],
+        'q90_equal_weights': [float(i + 1) for i in range(96)],
+        'targets': [i + 3 for i in range(96)],
+    }
+    expected_df = pd.DataFrame(expected_data, index=pd.date_range(start='2022-01-01 00:00:00', periods=96, freq='D'))
 
-# def test_calculate_equal_weights(mock_calculate_equal_weights_data):
-#     " Test calculate_equal_weights with valid data"
-#     df_test_norm_diff = mock_calculate_equal_weights_data
-#     df_equal_weights = calculate_equal_weights(df_test_norm_diff)
-#     expected_data = {
-#         'Q10': [0.1, 0.2, 0.3],
-#         'mean_prediction': [1.0, 1.1, 1.2],
-#         'Q90': [1.9, 2.0, 2.1],
-#         'diff_norm_measured': [1.5, 1.6, 1.7]
-#     }
-#     expected_df = pd.DataFrame(expected_data)
-#     pd.testing.assert_frame_equal(df_equal_weights, expected_df)
+    print(df_equal_weights)
+    print(expected_df)
+    pd.testing.assert_frame_equal(df_equal_weights, expected_df)
 
 # def test_calculate_equal_weights_missing_column(mock_calculate_equal_weights_missing_data):
 #     df_test_norm_diff = mock_calculate_equal_weights_missing_data
